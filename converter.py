651b37ceee97bc44f5c60bef99d4206e75b8c262
DeVontae <dhudso3@uwo.ca>
2023-02-21 13:39:03 -0500

    user can see topten and get a list of all and choose currency base

--- converter.py
+++ converter.py
@@ -7,11 +7,47 @@ import requests
 def interactions():
     quoteCurrency = print("Choose Quote Currency: ")
 
+def convertCurrency(baseCurrency, quoteCurrency, conversionRate):
+    conversionAmt = print(f"How much {baseCurrency} would you like to convert to {quoteCurrency}")
+    total = conversionAmt*conversionRate
 
+    return total
+
+
+def displayAllCurrencies():
+      #reading txt files -> found on stack
+      with open('Currencies.txt', 'r', encoding='utf-8') as f:
+        for line in f: 
+            currency = line.split()[0]  #gets the word at first index of eachline
+            print(currency)
+
+
+def displayCurrency():
+    #list top ten most common traded currencies
+    topCurrencies = ["USD", "EUR", "GBP", "CAD","AUD", "NZD", "CHF","JPY","CNY", "HKD"]
+    print("Select a Currency: ")
+    #lists them
+    for currency in topCurrencies:
+        print(currency)   
+    print("Enter 'ALL' to List All Currencies")
+    #takes user input
+    baseCurrency = input(">")
+    if baseCurrency == "ALL":
+        displayAllCurrencies()
+        baseCurrency = input(">")
+
+    
+    return baseCurrency    
+
+
+# def displayAll():
+#     d
+
+   
 
 #API call 
-def exchangerate():
-    baseCurrency = input("Choose base currency: ")
+def exchangerate(baseCurrency):
+    # baseCurrency = input("Choose base currency: ")
     #API key -> variable based on user input
     url = f"https://v6.exchangerate-api.com/v6/d1fb75f31e693824ef75c19f/latest/{baseCurrency}"
     #{baseCurrency} is base currency
@@ -31,7 +67,8 @@ def exchangerate():
 
 def main():
     #stuff goes here
-    exchangerate()
+    
+    exchangerate(displayCurrency())
 
 
 
